1493164a5081571c72592a2876ea11d3d596f47a
Romzorus <romzorus591@gmail.com>
2026-03-25 21:55:07 +0100

    basic impl : InventoryBuilder has no secrets (only references) -> build(SecretProvider) -> actionnable list of ManagedHosts

--- src/hosts/inventory.rs
+++ src/hosts/inventory.rs
@@ -1,1 +1,42 @@
+use serde::{Serialize, Deserialize};
 
+use crate::error::Error;
+use crate::secrets::SecretProvider;
+use crate::hosts::managed_host::ManagedHost;
+use crate::hosts::managed_host::ManagedHostBuilder;
+
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct InventoryBuilder {
+    hosts: Vec<ManagedHostBuilder>
+}
+
+impl InventoryBuilder {
+    pub fn build(self, secret_provider: &Option<SecretProvider>) -> Result<Inventory, Error> {
+        let mut hosts: Vec<ManagedHost> = Vec::new();
+
+        for host in self.hosts {
+            match host.build(secret_provider) {
+                Ok(managed_host) => {
+                    hosts.push(managed_host);
+                }
+                Err(detail) => {
+                    return Err(detail);
+                }
+            }
+        }
+
+        Ok(Inventory::from(hosts))
+    }
+}
+
+pub struct Inventory {
+    hosts: Vec<ManagedHost>
+}
+
+impl Inventory {
+    pub fn from(hosts: Vec<ManagedHost>) -> Self {
+        Inventory { hosts }
+    }
+}
