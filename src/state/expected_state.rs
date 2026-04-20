f25860c08ba5a0d8d51f46dec93e0c38dcd7bc26
Romzorus <romzorus591@gmail.com>
2026-04-20 22:12:28 +0200

    Introducing Parameter (either clear content or secret)

--- src/state/expected_state.rs
+++ src/state/expected_state.rs
@@ -1,7 +1,9 @@
-use std::collections::HashMap;
-
-use crate::{Error, state::attribute::Attribute};
+use crate::secrets::SecretProvider;
+use crate::{Error, secrets::SecretReference, state::attribute::Attribute};
+use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fmt::Debug;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
@@ -35,6 +37,80 @@ impl ExpectedState {
         ExpectedState {
             secrets_references: self.secrets_references.clone(),
             attributes: self.attributes.clone(),
+        }
+    }
+}
+
+#[derive(Clone, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum Parameter<T> {
+    Clear(T),
+    Secret(SecretReference),
+}
+
+impl<T> Debug for Parameter<T>
+where
+    T: Debug,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Parameter::Clear(content) => {
+                write!(f, "{:?}", content)
+            }
+            Parameter::Secret(content) => {
+                write!(f, "{:?}", content)
+            }
+        }
+    }
+}
+
+impl Parameter<String> {
+    pub fn inner_raw(
+        self,
+        optional_secret_provider: &Option<SecretProvider>,
+    ) -> Result<String, Error> {
+        match self {
+            Parameter::Clear(content) => Ok(content),
+            Parameter::Secret(secret_reference) => match optional_secret_provider {
+                Some(secret_provider) => {
+                    match secret_provider.get_secret_raw(secret_reference.sec_ref()) {
+                        Ok(secret) => Ok(secret.inner()),
+                        Err(error_detail) => {
+                            return Err(Error::FailedToGetSecret(format!("{:?}", error_detail)));
+                        }
+                    }
+                }
+                None => {
+                    return Err(Error::WrongInitialization(
+                        "Secrets are referenced but no SecretProvider to retrieve them from"
+                            .to_string(),
+                    ));
+                }
+            },
+        }
+    }
+}
+
+impl<T: DeserializeOwned> Parameter<T> {
+    pub fn inner(self, optional_secret_provider: &Option<SecretProvider>) -> Result<T, Error> {
+        match self {
+            Parameter::Clear(content) => Ok(content),
+            Parameter::Secret(secret_reference) => match optional_secret_provider {
+                Some(secret_provider) => {
+                    match secret_provider.get_secret_typed::<T>(secret_reference.sec_ref()) {
+                        Ok(secret) => Ok(secret.inner()),
+                        Err(error_detail) => {
+                            return Err(Error::FailedToGetSecret(format!("{:?}", error_detail)));
+                        }
+                    }
+                }
+                None => {
+                    return Err(Error::WrongInitialization(
+                        "Secrets are referenced but no SecretProvider to retrieve them from"
+                            .to_string(),
+                    ));
+                }
+            },
         }
     }
 }
